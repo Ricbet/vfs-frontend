f37eab9d83ab2a6e9948e0ea78941a2ec597a54a
Ricbet <18676769065@163.com>
2019-09-02 15:45:16 +0800

    new pages

--- demo/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import Page from "./page";
-import * as serviceWorker from "./serviceWorker";
-
-ReactDOM.render(<Page />, document.getElementById("root"));
-
-serviceWorker.unregister();
